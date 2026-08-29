91a2fb52cd706a4c0a21fdba14755788c0b7fcbe
noirbluu-source <noirbluu@gmail.com>
2026-08-29 22:42:00 +0300

    Refactor key event handling for back and menu actions

--- src/input/RemoteKeyFilter.cpp
+++ src/input/RemoteKeyFilter.cpp
@@ -7,14 +7,13 @@ bool RemoteKeyFilter::eventFilter(QObject *watched, QEvent *event) {
     if (event->type() == QEvent::KeyPress) {
         auto *keyEvent = static_cast<QKeyEvent *>(event);
         switch (keyEvent->key()) {
-            case Qt::Key_Back:
             case Qt::Key_Escape:
-            case Qt::Key_MediaPrevious:
-                emit backTriggered();
+            case Qt::Key_Back:
+                emit backRequested();
                 return true;
+            case Qt::Key_M:
             case Qt::Key_Menu:
-            case Qt::Key_Alt:
-                emit menuTriggered();
+                emit menuRequested();
                 return true;
             default:
                 break;

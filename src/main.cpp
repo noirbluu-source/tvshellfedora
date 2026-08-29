cc8d4f124bfcd97fd1cbe26b3b8d9eb093edc2e7
noirbluu-source <noirbluu@gmail.com>
2026-08-29 22:42:24 +0300

    Refactor event handling and application settings

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,40 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
-#include <QQuickWindow>
 #include "input/RemoteKeyFilter.hpp"
-#include "models/AppLauncherModel.hpp"
 
 int main(int argc, char *argv[]) {
-    // Force Wayland backend and Mesa hardware pipeline optimizations
+    // Standard Wayland and threaded Mesa render pipeline settings
     qputenv("QT_QPA_PLATFORM", "wayland");
     qputenv("QSG_RENDER_LOOP", "threaded");
 
     QGuiApplication app(argc, argv);
+    app.setApplicationName("tvshell");
+    app.setOrganizationName("TVShell");
 
     QQmlApplicationEngine engine;
-
-    AppLauncherModel launcherModel;
-    engine.rootContext()->setContextProperty("launcherModel", &launcherModel);
 
     RemoteKeyFilter keyFilter;
     app.installEventFilter(&keyFilter);
 
     const QUrl url(u"qrc:/qt/qml/TVShell/qml/Main.qml"_qs);
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url, &keyFilter](QObject *obj, const QUrl &objUrl) {
-        if (!obj && url == objUrl) {
-            QCoreApplication::exit(-1);
-        }
-        if (obj) {
-            QObject::connect(&keyFilter, SIGNAL(backTriggered()), obj, SLOT(handleGlobalBack()));
-            QObject::connect(&keyFilter, SIGNAL(menuTriggered()), obj, SLOT(handleGlobalMenu()));
-        }
-    }, Qt::QueuedConnection);
+
+    QObject::connect(
+        &engine,
+        &QQmlApplicationEngine::objectCreated,
+        &app,
+        [url, &keyFilter](QObject *obj, const QUrl &objUrl) {
+            if (!obj && url == objUrl) {
+                QCoreApplication::exit(-1);
+                return;
+            }
+            if (obj) {
+                QObject::connect(&keyFilter, SIGNAL(backRequested()), obj, SLOT(handleBack()));
+                QObject::connect(&keyFilter, SIGNAL(menuRequested()), obj, SLOT(handleMenu()));
+            }
+        },
+        Qt::QueuedConnection
+    );
 
     engine.load(url);
 
